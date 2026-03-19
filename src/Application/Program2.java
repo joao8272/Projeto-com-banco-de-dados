0ead538240664dfb9a78f17580e49900de62a0b3
Joao Pedro <pedro.arofilgueira@gmail.com>
2026-03-19 12:46:23 -0300

    add

---
+++ src/Application/Program2.java
@@ -0,0 +1,45 @@
+package Application;
+
+import model_Dao.DaoFactory;
+import model_Dao.DepartmentDao;
+import model_entities.Department;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST 1: department findById ====");
+        Department department = departmentDao.findById(3);
+        System.out.println(department);
+
+        System.out.println("\n=== TEST 2: department findAll ====");
+        List<Department> list = departmentDao.findAll();
+        for(Department obj : list){
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== TEST 3: department insert ====");
+        Department newDepartment = new Department(11, "Laptop");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
+
+        System.out.println("\n=== TEST 4: department update ====");
+        department = departmentDao.findById(1);
+        department.setName("Iphone");
+        departmentDao.update(department);
+        System.out.println("Update completed!");
+
+        System.out.println("\n=== TEST 5: department delete ====");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
+    }
+}
--- src/model_Dao/DaoFactory.java
+++ src/model_Dao/DaoFactory.java
@@ -1,11 +1,14 @@
 package model_Dao;
 
 import db.DB;
+import model_dao_impl.Department_dao_jdbc;
 import model_dao_impl.Seller_dao_jdbc;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
         return new Seller_dao_jdbc(DB.getConnection());
-
+    }
+    public static DepartmentDao createDepartmentDao(){
+        return new Department_dao_jdbc(DB.getConnection());
     }
 }
